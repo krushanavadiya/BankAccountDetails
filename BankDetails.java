e81c46735e9da3ba79bbf387069531c434fb1b71
Krusha Navadiya <9b66krusha.kesh@gmail.com>
2025-07-23 22:14:19 +0530

    Update BankDetails.java

--- BankDetails.java
+++ BankDetails.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class AccountManagement {
+public class BankDetails {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Account acc = null;
